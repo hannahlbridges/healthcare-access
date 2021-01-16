39025841a078904df7e1ae14232025f136a0ea95
Carlisle Willard <cwalkerwillard@gmail.com>
2021-01-16 09:27:49 -0600

    updated requirements file

--- app2.py
+++ app2.py
@@ -1,13 +1,25 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
 from os import environ
+import pandas as pd
+import numpy as np
+from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
     'DATABASE_URL', 'sqlite:///notepad.sqlite')
 
+engine = create_engine(environ.get(
+    'DATABASE_URL', 'sqlite:///notepad.sqlite'))
+Base = automap_base()
+Base.prepare(engine, reflect =True)
 db = SQLAlchemy(app)
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
